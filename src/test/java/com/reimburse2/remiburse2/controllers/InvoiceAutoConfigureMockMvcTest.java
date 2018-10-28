75faf6102eddabed3a87528fac3b617a0cd99dfc
Mumtaz.Khan <mumtaz.khan@daugherty.com>
2018-10-28 09:23:39 -0500

    added auto configure AutoConfigureMockMvc test

--- src/test/java/com/reimburse2/remiburse2/controllers/InvoiceAutoConfigureMockMvcTest.java
+++ src/test/java/com/reimburse2/remiburse2/controllers/InvoiceAutoConfigureMockMvcTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,8 +12,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest (InvoiceController.class)
-public class InvoiceWebMvcTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InvoiceAutoConfigureMockMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -24,17 +23,5 @@ public class InvoiceWebMvcTest {
     public void invoiceControllerShouldReturnStatusOK() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/invoice"))
                 .andExpect(status().isOk());
-    }
-
-    @Test
-    public void invoiceContollerShouldLoadGetAnInvoice() throws Exception {
-        Object o = this.mockMvc.perform(MockMvcRequestBuilders.get("/invoice"));
-        System.out.println(o);
-    }
-
-    @Test
-    public void invoiceContollerShouldLoadAllInvoices() throws Exception {
-        Object o = this.mockMvc.perform(MockMvcRequestBuilders.get("/invoices"));
-        System.out.println(o);
     }
 }
